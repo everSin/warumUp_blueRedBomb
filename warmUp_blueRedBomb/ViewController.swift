714a50e8d84cbd8fb9dc99b438aeaef0e6a0b9ba
Severin Bretscher <severin.bretscher@fewclicks.de>
2015-09-12 00:44:47 +0200

    Initial Commit

---
+++ warmUp_blueRedBomb/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  warmUp_blueRedBomb
+//
+//  Created by Severin Bretscher on 12/09/15.
+//  Copyright (c) 2015 Severin Bretscher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
