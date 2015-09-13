7ce3f695105b35e3bf1a2ff17c7ce3eb9149e8d9
Severin Bretscher <eversin@me.com>
2015-09-13 13:38:18 +0200

    adding the project to git

--- warmUp_blueRedBomb/ViewController.swift
+++ warmUp_blueRedBomb/ViewController.swift
@@ -10,6 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+	@IBOutlet weak var blueBomb: UIImageView!
+	@IBOutlet weak var redBomb: UIImageView!
+
+	
+	@IBAction func blueButton(sender: UIButton) {
+		
+		if blueBomb.hidden == false{
+			
+		blueBomb.hidden = true
+		sender.setTitle("Show Blue Bomb", forState: UIControlState.Normal)
+		
+		}else{
+			
+			blueBomb.hidden = false
+			sender.setTitle("Hide Blue Bomb", forState: UIControlState.Normal)
+			
+		}
+	}
+	
+	@IBAction func redButton(sender: UIButton) {
+		
+		if redBomb.hidden == false {
+			
+			redBomb.hidden = true
+			sender.setTitle("Show Blue Bomb", forState: UIControlState.Normal)
+			
+		}else{
+			
+			redBomb.hidden = false
+			sender.setTitle("Hide Blue Bomb", forState: UIControlState.Normal)
+		
+			
+		}
+		
+	}
+	
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
